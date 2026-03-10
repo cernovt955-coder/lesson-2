6dd1b5b11524ed8ea1c1066966918ecea15ddb1f
cernovt955-coder <cernovt955@gmail.com>
2026-03-10 12:05:18 +0500

    lesson_3_task_2.py

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,7 @@
+catalog = []
+phone1 = Smartphone("Марка1", "Модель1", "+79000000001")
+# ... (создать остальные телефоны) ...
+catalog.extend([phone1, phone2, phone3, phone4, phone5])
+
+for phone in catalog:
+    print(f"{phone.brand} - {phone.model}. {phone.phone_number}")
