0a635986e7631220acd15e3bfc6004796489b0e5
cernovt955-coder <cernovt955@gmail.com>
2026-03-12 12:04:05 +0500

    Mailing.py

--- 03_lesson/Mailing.py
+++ 03_lesson/Mailing.py
@@ -1,6 +1,6 @@
-class Mailing
-def __init__(to_address,from_address,cost,track)
-self.from_address = from_address
- self.to_address = to_address
-self.cost = cost
-self.track = track
+class Mailing:
+    def __init__(self, to_address, from_address, cost, track):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
