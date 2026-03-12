5481f36b09fe4e7214b67de1eb4aa6de6a8ec49f
cernovt955-coder <cernovt955@gmail.com>
2026-03-12 12:02:33 +0500

    lesson_3_task_2.py

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,6 +1,7 @@
 catalog = []
 phone1 = Smartphone("Марка1", "Модель1", "+79000000001")
-# ... (создать остальные телефоны) ...
+phone2 = Smartphone("Марка2", "Модель2", "+79000000002")
+# ... и так далее для пяти телефонов
 catalog.extend([phone1, phone2, phone3, phone4, phone5])
 
 for phone in catalog:
