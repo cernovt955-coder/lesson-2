9e91cb301a201727ff70b5b7f320fd4c217ffeb2
cernovt955-coder <cernovt955@gmail.com>
2026-03-18 13:07:13 +0500

    test_string_utils.py

--- "04_lesson/test_string_utils.py
+++ "04_lesson/test_string_utils.py
@@ -1,1 +1,15 @@
+import pytest
+from string_utils import StringUtils
+Позитивные сценарии 
+def capitalize(self, string: str) -> str:
+  ОР: Ввод данных Skypro-Skypro вводится в систему
+  def trim(self, string: str) -> str:
+    ОР:Ввод данных Skypro-Skypro вводится в систему
+def trim(self, string: str) -> str:
+   Ввод данных trim -trim вводится в систему
 
+Негативные сценарии
+def contains(self, string: str, symbol: str) -> bool:
+  ОР: Ошибка ввода данных в систему 
+  def delete_symbol(self, string: str, symbol: str) -> str:
+    ОР: Ошибка ввод данных в систему
