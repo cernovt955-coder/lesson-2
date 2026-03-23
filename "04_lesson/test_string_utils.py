f6c5cf857abb9a5b3b0963400482782d1a4e8797
cernovt955-coder <cernovt955@gmail.com>
2026-03-23 13:00:47 +0500

    test_string_utils.py

--- "04_lesson/test_string_utils.py
+++ "04_lesson/test_string_utils.py
@@ -1,15 +1,26 @@
-import pytest
-from string_utils import StringUtils
-Позитивные сценарии 
-def capitalize(self, string: str) -> str:
-  ОР: Ввод данных Skypro-Skypro вводится в систему
-  def trim(self, string: str) -> str:
-    ОР:Ввод данных Skypro-Skypro вводится в систему
-def trim(self, string: str) -> str:
-   Ввод данных trim -trim вводится в систему
+utils = StringUtils()
 
-Негативные сценарии
-def contains(self, string: str, symbol: str) -> bool:
-  ОР: Ошибка ввода данных в систему 
-  def delete_symbol(self, string: str, symbol: str) -> str:
-    ОР: Ошибка ввод данных в систему
+utils.trim("skypro")
+Позитивный тест
+def test_trim_positive(): 
+    assert utils.trim("skypro") == "Skypro"
+Негативный тест 
+def test_trim_empty(): 
+    assert utils.capitalize("") == ""
+ 
+utils.contains("true" "false")
+Позмтивный тест 
+def test_contains_positive(): 
+    assert utils.contains(""true" "false") == "true" "false
+Неагтивный тест
+def test__containsempty(): 
+    assert utils.contains("") == ""
+
+utils.delete_symbol("skypro")
+Позитивный тест 
+def test_delete_symbol_positive(): 
+ assert utils.delete_symbol("skypro") == "Skypro"
+ 
+Негативный тест 
+def testdelete_symbol__empty(): 
+    assert utils.delete_symbol("") == ""
