43f748bf533036c7d6f7f45d0fec34edf9117487
cernovt955-coder <cernovt955@gmail.com>
2026-03-31 13:03:41 +0500

    test_string_utils.py

--- "04_lesson/test_string_utils.py
+++ "04_lesson/test_string_utils.py
@@ -5,26 +5,26 @@ utils = StringUtils()
 
 
 utils.trim("skypro")
-Позитивный тест
-# def test_trim_positive(): 
+# Позитивный тест
+ "def test_trim_positive()": 
     assert utils.trim("skypro") == "Skypro"
-Негативный тест 
-def test_trim_empty(): 
+# Негативный тест 
+"def test_trim_empty()": 
     assert utils.capitalize("") == ""
  
 utils.contains("true" "false")
-Позмтивный тест 
-# def test_contains_positive(): 
+# Позмтивный тест 
+ "def test_contains_positive()": 
    assert utils.contains("SkyPro", "S") == True
-Неагтивный тест
-def test__containsempty(): 
+# Неагтивный тест
+"def test__containsempty()": 
     assert utils.contains("") == ""
 
 utils.delete_symbol("skypro")
-Позитивный тест 
-# def test_delete_symbol_positive(): 
+# Позитивный тест 
+ "def test_delete_symbol_positive()": 
 assert utils.delete_symbol("SkyPro", "k") == "SyPro"
  
-Негативный тест 
-def testdelete_symbol__empty(): 
+# Негативный тест 
+"def testdelete_symbol__empty()": 
     assert utils.delete_symbol("") == ""
