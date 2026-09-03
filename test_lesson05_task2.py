eb766f3a167ba53fe977a8a6634f18bfb46af5ff
cernovt955-coder <cernovt955@gmail.com>
2026-09-03 14:13:58 +0500

    Update test_lesson05_task2.py

--- test_lesson05_task2.py
+++ test_lesson05_task2.py
@@ -6,6 +6,12 @@ def test_form_submission():
     driver = webdriver.Chrome()
     driver.get("https://httpbin.qa-territory.online/forms/post")
 
-    # Ваш код здесь
+    name_input = driver.find_element(By.NAME, "custname")
+    name_input.send_keys("Андрей")
+
+    submit_button = driver.find_element(By.XPATH, "//button[text()='Submit']")
+    submit_button.click()
+
+    assert driver.current_url != "https://httpbin.qa-territory.online/forms/post"
 
     driver.quit()
