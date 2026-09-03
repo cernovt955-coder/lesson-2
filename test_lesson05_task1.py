b0d4c8d15d4897dd488866e26ad2164159ae90a8
cernovt955-coder <cernovt955@gmail.com>
2026-09-03 13:38:46 +0500

    Update test_lesson05_task1.py

--- test_lesson05_task1.py
+++ test_lesson05_task1.py
@@ -4,7 +4,15 @@ from selenium.webdriver.common.by import By
 
 def test_navigation():
     driver = webdriver.Chrome()
+    driver.get("https://httpbin.qa-territory.online")
 
-    # Ваш код здесь
+    link = driver.find_element(By.LINK_TEXT, "HTML Form")
+    link.click()
+
+    assert driver.current_url == "https://httpbin.qa-territory.online/forms/post"
+
+    driver.back()
+
+    assert driver.current_url == "https://httpbin.qa-territory.online/"
 
     driver.quit()
